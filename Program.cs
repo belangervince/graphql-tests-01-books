7f677c6feb2ae26495f7df3a869d748600b05ce3
belangervince <belangervince@outlook.com>
2024-03-26 21:05:52 -0400

    subscriptions and mutations

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQLServer()
     .AddQueryType<Query>()
-    .AddInterfaceType<IReadingMaterials>();
+    .AddInterfaceType<IReadingMaterials>()
+    .AddMutationType<Mutation>()
+    .AddSubscriptionType<Subscription>();
+
+builder.Services.AddInMemorySubscriptions();
     
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGraphQL();
+app.UseWebSockets();
 
 app.Run();
